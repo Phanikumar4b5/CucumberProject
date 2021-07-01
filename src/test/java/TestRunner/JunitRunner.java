7a8c0c032b3e48aa29c39cb89ce22a1f51013f62
phanikumar4b5 <phanikumar4b5@gmail.com>
2021-07-01 17:44:50 +0530

    Committed

--- src/test/java/TestRunner/JunitRunner.java
+++ src/test/java/TestRunner/JunitRunner.java
@@ -1,26 +1,20 @@
-package TestRunner;
-
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
- features = "Features"
-		 ,glue={"StepDefinition"},
-				 monochrome = true,
-		 dryRun=false
-		 //strict=false,
-		 //tags= {"@FunctionalTest","~@SmokeTest","~@RegrssionTest"},
-		//format= {"pretty"},
-		// plugin= {"usage"}
-		 /*plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
-				 "junit:target/cucumber-reports/Cucumber.xml",
-				 "html:target/cucumber-reports"}*/
-				 //plugin = {"html:target/cucumber-reports"}
- 		//format= {"json:C:\\\\Users\\\\791733\\\\Documents"}
- )
-
-public class JunitRunner {
-}
+/*
+ * package TestRunner;
+ * 
+ * import org.junit.runner.RunWith;
+ * 
+ * import cucumber.api.CucumberOptions; import cucumber.api.junit.Cucumber;
+ * 
+ * @RunWith(Cucumber.class)
+ * 
+ * @CucumberOptions( features = "Features" ,glue={"StepDefinition"}, monochrome
+ * = true, dryRun=false //strict=false, //tags=
+ * {"@FunctionalTest","~@SmokeTest","~@RegrssionTest"}, //format= {"pretty"}, //
+ * plugin= {"usage"} plugin = { "pretty",
+ * "json:target/cucumber-reports/Cucumber.json",
+ * "junit:target/cucumber-reports/Cucumber.xml", "html:target/cucumber-reports"}
+ * //plugin = {"html:target/cucumber-reports"} //format=
+ * {"json:C:\\\\Users\\\\791733\\\\Documents"} )
+ * 
+ * public class JunitRunner { }
+ */
