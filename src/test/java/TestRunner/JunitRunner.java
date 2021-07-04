6e9eddd6a3d820add57140befc1e3ba55ee5c2ff
phanikumar4b5 <phanikumar4b5@gmail.com>
2021-07-04 19:43:01 +0530

    committed

--- src/test/java/TestRunner/JunitRunner.java
+++ src/test/java/TestRunner/JunitRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
   
 @CucumberOptions(
 		 features = "Features"
